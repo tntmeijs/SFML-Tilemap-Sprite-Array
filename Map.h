c69db9f713911845fa5669fb16cc9dc8f20facca
Tahar Meijs (Taharrie) <taharrie@users.noreply.github.com>
2015-12-30 11:42:36 +0100

    Update Map.h

--- Map.h
+++ Map.h
@@ -12,13 +12,19 @@ private:
 	void initIntRect();
 
 private:
+	// In the constructor, specify these constants
+	// They are important
 	const int tileWidth;
 	const int tileHeight;
 
+	// This textures holds the textures for the tiles
+	// It should be a single image with multiple tiles (like a spritesheet)
 	sf::Texture tileSheet;
 
 	sf::Sprite tile;
 
+	// Add / remove these in order to add more tiles
+	// This depends on the amount of tiles in your texture
 	sf::IntRect pinkPath;
 	sf::IntRect woodenBox;
 	sf::IntRect grass;
